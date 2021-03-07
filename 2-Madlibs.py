f6a6e129e2a8fe8a20c330ada76e5087a22ce786
DrPotatoV <79384355+DrPotatoV@users.noreply.github.com>
2021-03-06 19:03:10 -0800

    Add files via upload

---
+++ 2-Madlibs.py
@@ -0,0 +1,16 @@
+    # All the questions that the program asks the user
+    accion1 = input("Introduce un verbo de infinitivo: ")
+    animal = input("Escribe un animal: ")
+    accion2 = input("Escribe otro verbo infinitivo: ")
+    nombre1 = input("Introduce un nombre: ")
+    nombre2 = input("Introduzca otro nombre: ")
+    pro = input("Introduzca una profesion: ")
+    lugar = input("Introduzca un lugar: ")
+
+    # Displays the story based on the users input
+    print ("------------------------------------------")
+    print ("Algunas veces cuando salgo a "+ accion1 +" me topo al " + animal + " del vecino")
+    print ("Entonces comienzo a " + accion2 + " para que no corra a morderme")
+    print (nombre1 + " me decia siempre: nunca vuelvas a pasar por la casa de" + nombre2)
+    print ("o tendre que llamar al " + pro + " para que te lleven a" + lugar + " de nuevo")
+    print ("------------------------------------------")
---
+++ paroimpar.py
@@ -0,0 +1,7 @@
+#Par o impar
+a = int(input('Introduzca un numero \n'))
+
+if a % 2 == 0:
+    print('El número', a, 'es par.')
+else:
+    print('El número', a, 'es impar.')
